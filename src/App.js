b15fc9e21fb23f1165e37b0c0d68995ac25d26e9
tajkalaski <laski.tajka@gmail.com>
2021-01-18 14:32:22 +0100

    eslint, prettier, husky and lint-staged configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Incididunt amet culpa elit sit proident. Do esse sint eu in tempor cupidatat et ullamco
+          excepteur ipsum dolore nisi. Laboris tempor aliquip ipsum pariatur nulla ad proident esse
+          voluptate nulla eu cillum officia sint. Dolore adipisicing amet anim velit tempor eu velit
+          amet cillum ea culpa enim dolor incididunt. Aliqua ea non sunt elit anim qui sunt quis
+          sint et nulla. Incididunt id irure nulla voluptate dolore nostrud nisi ut irure dolor id
+          anim non mollit. Consequat ut do esse voluptate esse reprehenderit elit magna elit
+          occaecat.
         </a>
       </header>
     </div>
